ef8c1fa9d8762d04134990f539d3d7207464752c
K2x141b <xaviencedric@gmail.com>
2024-06-03 17:55:16 +0200

    string fixing

--- python/socket_server.py
+++ python/socket_server.py
@@ -151,7 +151,7 @@ class Client(Thread):
                             tap_dash += 0
                         else:
                             tap_dash += 1
-                        reply += tap_dash
+                        reply += str(tap_dash)
                         #reply = tapping_keys[str(i)]
                     elif data == "feed_paper_in_inc":
                         lego_printer.manual_paper_feed_inc(1)
